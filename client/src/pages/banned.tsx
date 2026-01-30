063caaf3c52a7cc38f583f7ea237593422c61595
John/Dev <jgarland13191@gmail.com>
2026-01-30 01:20:31 +0000

    Create banned.tsx

---
+++ client/src/pages/banned.tsx
@@ -0,0 +1,3 @@
+// this is the page that users will see when they are banned
+// it will display the reason for the ban, when they were banned and why they were banned
+// it will also display a button to message the server admin to appeal the ban if appeals are enabled
